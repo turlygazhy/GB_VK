386ecd1a5bf036b3d9f90531b5db88431dd52a32
turlygazhy <erasul_32@mail.ru>
2021-08-23 17:23:57 +0600

    Initial Commit

---
+++ GB_VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB_VK
+//
+//  Created by Ерасыл Турлыгажы on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
